9a4f85aca45599e5c659a17d436fe49903f77adf
necrofinder <necrofinder@gmail.com>
2015-06-24 14:25:32 -0500

    Update Permissions.java

--- ChatManagerPlus/src/ct/ChatManagerPlus/Permissions.java
+++ ChatManagerPlus/src/ct/ChatManagerPlus/Permissions.java
@@ -8,8 +8,12 @@ public class Permissions {
 	public static Permission command_muteplayer = new Permission("ChatManagerPlus.mute");
 	public static Permission command_clearchat = new Permission("ChatManagerPlus.clearchat");
 	public static Permission command_staffchat = new Permission("ChatManagerPlus.staffchat");
-	public static Permission passive_colorchat = new Permission("ChatManagerPlus.coloredchat");
 	public static Permission command_broadcast = new Permission("ChatManagerPlus.broadcast");
 	public static Permission command_cooldown = new Permission("ChatManagerPlus.cooldownchat");
 
+	public static Permission bypass_lockchat = new Permission("ChatManagerPlus.lockchatbypass");
+	public static Permission bypass_mute = new Permission("ChatManagerPlus.mutebypass");
+	public static Permission bypass_cooldown = new Permission("ChatManagerPlus.cooldownchatbypass");
+
+	public static Permission passive_colorchat = new Permission("ChatManagerPlus.coloredchat");
 }
